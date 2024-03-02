6301a19b58ed2e5b699c350858cba05b2269aeb1
Ron Amihai <ron.amihai@cycode.com>
2024-03-02 11:54:28 +0200

    Initial Commit

---
+++ src/main/java/cloudcomputing2024/smarthouse/trafficmonitorservice/TrafficMonitorServiceApplication.java
@@ -0,0 +1,13 @@
+package cloudcomputing2024.smarthouse.trafficmonitorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficMonitorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficMonitorServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cloudcomputing2024/smarthouse/trafficmonitorservice/TrafficMonitorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cloudcomputing2024.smarthouse.trafficmonitorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficMonitorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
