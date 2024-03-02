96c019a178236937a8262b328f48f24c6dd53c60
LiorAriely <lior.ariely@gmail.com>
2024-03-02 19:00:16 +0200

    adding start of KAFKA connection

---
+++ src/main/java/cloudcomputing2024/smarthouse/trafficmonitorservice/infrastructure/KafkaProducer.java
@@ -0,0 +1,17 @@
+package cloudcomputing2024.smarthouse.trafficmonitorservice.infrastructure;
+
+import cloudcomputing2024.smarthouse.trafficmonitorservice.messages.ServiceTopicDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaProducer {
+
+    @Autowired
+    private KafkaTemplate<String, ServiceTopicDefinition> kafkaTemplate;
+
+    public void send(ServiceTopicDefinition message) {
+        kafkaTemplate.send("ServiceDefinitionTopic", message.getServiceName(), message);
+    }
+}
