8184effb52d0d941aee3cee440e6f2eab6ba7cf9
niv-e <niveliyahu25@gmail.com>
2024-03-02 18:35:45 +0200

    Change MongoDB to nonreactive version

--- src/main/java/cloudcomputing2024/smarthouse/trafficmonitorservice/infrastructure/ServiceTopicDefinitionRepository.java
+++ src/main/java/cloudcomputing2024/smarthouse/trafficmonitorservice/infrastructure/ServiceTopicDefinitionRepository.java
@@ -2,11 +2,11 @@ package cloudcomputing2024.smarthouse.trafficmonitorservice.infrastructure;
 
 import cloudcomputing2024.smarthouse.trafficmonitorservice.messages.ServiceTopicDefinition;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Component
+@Repository
 public interface ServiceTopicDefinitionRepository extends MongoRepository<ServiceTopicDefinition, String> {
     public List<ServiceTopicDefinition> findByServiceName(String firstName);
     void deleteByServiceNameAndTopic(ServiceTopicDefinition serviceTopicDefinition);
--- src/main/java/cloudcomputing2024/smarthouse/trafficmonitorservice/messages/ServiceTopicDefinition.java
+++ src/main/java/cloudcomputing2024/smarthouse/trafficmonitorservice/messages/ServiceTopicDefinition.java
@@ -1,9 +1,10 @@
 package cloudcomputing2024.smarthouse.trafficmonitorservice.messages;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
-
+@Document
 public record ServiceTopicDefinition(@Id String serviceName,
                                      String topic,
                                      int maxRequestsPerMinute,
