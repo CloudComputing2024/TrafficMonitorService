8f00ed750c27f6a126652bd5ca7f4866f9a36d11
Assaf Ariely <assaf.ariely@sap.com>
2024-03-09 13:57:53 +0200

    Removing Rsocket action and implementing api strategy

--- src/main/java/cloudcomputing2024/smarthouse/trafficmonitorservice/notifications/strategies/NotificationStrategyProvider.java
+++ src/main/java/cloudcomputing2024/smarthouse/trafficmonitorservice/notifications/strategies/NotificationStrategyProvider.java
@@ -5,19 +5,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationStrategyProvider implements INotificationStrategyProvider {
-    private final RSocketNotificationStrategy rSocketNotificationStrategy;
     private final KafkaTopicNotificationStrategy kafkaTopicNotificationStrategy;
     private final ApiRequestNotificationStrategy apiRequestNotificationStrategy;
 
-    public NotificationStrategyProvider(RSocketNotificationStrategy rSocketNotificationStrategy, KafkaTopicNotificationStrategy kafkaTopicNotificationStrategy, ApiRequestNotificationStrategy apiRequestNotificationStrategy) {
-        this.rSocketNotificationStrategy = rSocketNotificationStrategy;
+    public NotificationStrategyProvider(KafkaTopicNotificationStrategy kafkaTopicNotificationStrategy, ApiRequestNotificationStrategy apiRequestNotificationStrategy) {
         this.kafkaTopicNotificationStrategy = kafkaTopicNotificationStrategy;
         this.apiRequestNotificationStrategy = apiRequestNotificationStrategy;
     }
 
     public NotificationStrategy getStrategy(NotificationType notificationType) {
         return switch (notificationType) {
-            case RSocket -> rSocketNotificationStrategy;
             case KafkaTopic -> kafkaTopicNotificationStrategy;
             case ApiRequest -> apiRequestNotificationStrategy;
         };
